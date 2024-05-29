e42d4a01925f0eaf39b7183f4a685a6c29e2c787
boggulabhargavateja <142088218+boggulabhargavateja@users.noreply.github.com>
2024-05-29 22:57:09 +0530

    Update api_data_collector.py

--- api_data_collector.py
+++ api_data_collector.py
@@ -1,1 +1,8 @@
+import requests
+
+def get_company_data(symbol):
+    api_key = 'YOUR_API_KEY'
+    url = f'https://www.alphavantage.co/query?function=OVERVIEW&symbol={symbol}&apikey={api_key}'
+    response = requests.get(url)
+    return response.json()
 
